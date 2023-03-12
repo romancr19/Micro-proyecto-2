796329f85c7590cd9612578776f0d775e6fb5dc8
AndresBLV <77251658+AndresBLV@users.noreply.github.com>
2023-03-12 11:32:56 -0400

    Firebase

---
+++ Microproyecto2/src/firebase/firebase.jsx
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC0Tz9mCh4PfXUPSB0NlKDloQpA5OZ6Gj0",
+    authDomain: "vite-react-4d06e.firebaseapp.com",
+    projectId: "vite-react-4d06e",
+    storageBucket: "vite-react-4d06e.appspot.com",
+    messagingSenderId: "1031591097644",
+    appId: "1:1031591097644:web:a8e3f81a6ad603dfa6e889"
+  };
+
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
